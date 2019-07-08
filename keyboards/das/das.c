12bc030e0e3e2f2330e36560a0c24accc0bcc269
matthew-nichols <matthew.nichols@mx.com>
2019-07-07 22:05:10 -0600

    Handle USB state leds for das keyboard

--- keyboards/das/das.c
+++ keyboards/das/das.c
@@ -1,4 +1,6 @@
 #include "das.h"
+#include "hal.h"
+#include "teensy.h"
 
 __attribute__ ((weak))
 void matrix_init_user(void) {
@@ -15,3 +17,43 @@ void matrix_init_kb(void) {
 void matrix_scan_kb(void) {
   matrix_scan_user();
 };
+
+#define CAPS_LOCK_LED_PIN  TEENSY_PIN28
+#define CAPS_LOCK_LED_PORT TEENSY_PIN28_IOPORT
+#define NUM_LOCK_LED_PIN   TEENSY_PIN29
+#define NUM_LOCK_LED_PORT  TEENSY_PIN29_IOPORT
+#define SCROLL_LOCK_LED_PIN   TEENSY_PIN30
+#define SCROLL_LOCK_LED_PORT  TEENSY_PIN30_IOPORT
+
+void keyboard_pre_init_kb(void) {
+    // Set our LED pins as output
+    palSetPadMode(TEENSY_PIN28_IOPORT, TEENSY_PIN28, PAL_MODE_OUTPUT_PUSHPULL);
+    palSetPadMode(TEENSY_PIN29_IOPORT, TEENSY_PIN29, PAL_MODE_OUTPUT_PUSHPULL);
+    palSetPadMode(TEENSY_PIN30_IOPORT, TEENSY_PIN30, PAL_MODE_OUTPUT_PUSHPULL);
+
+    keyboard_pre_init_user();
+}
+
+void led_set_kb(uint8_t usb_led) {
+    if (IS_LED_ON(usb_led, USB_LED_NUM_LOCK)) {
+        palSetPad(NUM_LOCK_LED_PORT, NUM_LOCK_LED_PIN);
+    } else {
+        palClearPad(NUM_LOCK_LED_PORT, NUM_LOCK_LED_PIN);
+    }
+
+    if (IS_LED_ON(usb_led, USB_LED_CAPS_LOCK)) {
+        onboardLedOn();
+        palSetPad(CAPS_LOCK_LED_PORT, CAPS_LOCK_LED_PIN);
+    } else {
+        onboardLedOff();
+        palClearPad(CAPS_LOCK_LED_PORT, CAPS_LOCK_LED_PIN);
+    }
+
+    if (IS_LED_ON(usb_led, USB_LED_SCROLL_LOCK)) {
+        palSetPad(SCROLL_LOCK_LED_PORT, SCROLL_LOCK_LED_PIN);
+    } else {
+        palClearPad(SCROLL_LOCK_LED_PORT, SCROLL_LOCK_LED_PIN);
+    }
+
+    led_set_user(usb_led);
+}
