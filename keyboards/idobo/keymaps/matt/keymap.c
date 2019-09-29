3988ce045534402ac406ceccc590768218dcae19
matthew-nichols <matthew.nichols@mx.com>
2019-09-29 12:47:19 -0600

    Initial preonic layout

--- keyboards/idobo/keymaps/matt/keymap.c
+++ keyboards/idobo/keymaps/matt/keymap.c
@@ -25,21 +25,37 @@ enum custom_keycodes {
 
 #define _QW 0
 #define _FN 1
+#define _MOVE 2
+#define _ADJUST 3
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  [_QW] = LAYOUT_ortho_5x15( \
-    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_MINS, SHRUG,   KC_EQL,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC, KC_BSLS, KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_QUOT,
-    KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_VOLU, KC_DEL,  KC_MFFD, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_VOLD, KC_MPLY, KC_MRWD, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   KC_RSFT,
-    MO(_FN), MO(_FN), KC_LCTL, KC_LALT, KC_LGUI, KC_SPC,  KC_SPC,  MO(_FN), KC_SPC,  KC_SPC,  KC_RGUI, KC_SLSH, KC_LEFT, KC_DOWN, KC_RGHT
+    LT(_ADJUST, KC_ESC),  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,        KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
+    KC_TAB,               KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    SHRUG,   KC_DEL,  KC_Y,    KC_U,        KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,
+    LT(_MOVE, KC_ESC),    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_MPLY, KC_VOLU, KC_H,    KC_J,        KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
+    KC_LSFT,              KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_MFFD, KC_VOLD, KC_N,    KC_M,        KC_COMM, KC_DOT,  KC_SLSH, KC_UP,   KC_RSFT,
+    KC_LCTL,              KC_LCTL, KC_LALT, KC_LGUI, KC_LGUI, KC_SPC,  KC_SPC,  KC_SPC,  KC_SPC,  KC_RGUI,     KC_RGUI, KC_BSLS, KC_LEFT, KC_DOWN, KC_RGHT
   ),
   [_FN] = LAYOUT_ortho_5x15( \
-    _______,  KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      _______,   _______,   _______,   KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_F11,
+    _______,  _______,    KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,     KC_F6,     KC_F7,     KC_F8,      KC_F9,      KC_F10,     KC_F11,     KC_F12,     KC_DEL,
+    _______,  _______,    _______,    _______,    _______,    _______,    ESHRUG,    _______,   _______,   _______,    _______,    _______,    _______,    _______,    _______,
+    _______,  _______,    _______,    _______,    _______,    _______,    RGB_MOD,   RGB_M_P,   _______,   _______,    _______,    _______,    _______,    _______,    _______,
+    _______,  _______,    _______,    _______,    _______,    MD_BOOT,    RGB_HUI,   _______,   _______,   _______,    _______,    _______,    _______,    _______,    _______,
+    _______,  _______,    _______,    _______,    _______,    _______,    RGB_HUD,   _______,   _______,   _______,    _______,    _______,    _______,    _______,    _______
+  ),
+  [_MOVE] = LAYOUT_ortho_5x15( \
+    _______,  _______,    KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,     KC_F6,     KC_F7,     KC_F8,      KC_F9,      KC_F10,     KC_F11,     KC_F12,     KC_DEL,
     _______,  _______,    _______,    _______,    _______,    _______,    _______,   _______,   _______,   _______,    _______,    _______,    _______,    _______,    _______,
+    _______,  _______,    _______,    _______,    _______,    _______,    _______,   _______,   KC_LEFT,   KC_DOWN,    KC_UP,      KC_RGHT,    _______,    _______,    _______,
+    _______,  _______,    _______,    _______,    _______,    _______,    KC_MRWD,   _______,   _______,   _______,    _______,    _______,    _______,    _______,    _______,
+    _______,  _______,    _______,    _______,    _______,    _______,    _______,   _______,   _______,   _______,    _______,    _______,    _______,    _______,    _______
+  ),
+  [_ADJUST] = LAYOUT_ortho_5x15( \
     _______,  _______,    _______,    _______,    _______,    _______,    _______,   _______,   _______,   _______,    _______,    _______,    _______,    _______,    _______,
-    _______,  _______,    _______,    _______,    _______,    _______,    _______,   _______,   _______,   _______,    _______,    _______,    _______,    _______,    _______,
-    MD_BOOT,  _______,    _______,    _______,    _______,    _______,    _______,   _______,   _______,   _______,    _______,    _______,    _______,    _______,    _______
+    _______,  _______,    _______,    _______,    _______,    _______,    RGB_MOD,   RGB_M_P,   _______,   _______,    _______,    _______,    _______,    _______,    _______,
+    _______,  _______,    _______,    _______,    _______,    _______,    RGB_HUI,   _______,   _______,   _______,    _______,    _______,    _______,    _______,    _______,
+    _______,  _______,    _______,    _______,    _______,    MD_BOOT,    RGB_HUD,   _______,   _______,   _______,    _______,    _______,    _______,    _______,    _______,
+    _______,  _______,    _______,    _______,    _______,    _______,    _______,   _______,   _______,   _______,    _______,    _______,    _______,    _______,    _______
   ),
 };
 
@@ -49,13 +65,13 @@ bool process_record_keymap(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
     case SHRUG:
       if (record->event.pressed) {
-        SEND_STRING("¯\\_(ツ)_/¯");
+        SEND_STRING("shrug");
       } else {
       }
       break;
     case ESHRUG:
       if (record->event.pressed) {
-        SEND_STRING("¯\\\\\\_(ツ)\\_\\/¯");
+        SEND_STRING("eshrug");
       } else {
       }
       break;
@@ -64,7 +80,6 @@ bool process_record_keymap(uint16_t keycode, keyrecord_t *record) {
           key_timer = timer_read32();
       } else {
           if (timer_elapsed32(key_timer) >= 500) {
-              SEND_STRING("¯\\_(ツ)_/¯");
               reset_keyboard();
           }
       }
