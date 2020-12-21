ef5cd5938b1b94e401600ddc67a3cd2c4d61b0ac
matthew-nichols <matthew.nichols@mx.com>
2020-12-21 10:33:08 -0700

    Add tokyo keymap and other personal items

--- keyboards/idobo/keymaps/default/keymap.c
+++ keyboards/idobo/keymaps/default/keymap.c
@@ -15,40 +15,44 @@
  */
 #include QMK_KEYBOARD_H
 
+#define _BASE 0
+#define _FN 1
+
 // Defines the keycodes used by our macros in process_record_user
 enum custom_keycodes {
-  QMKBEST = SAFE_RANGE,
-  QMKURL
+  MD_BOOT = SAFE_RANGE
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
- [0] = LAYOUT_ortho_5x15( \
-    KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_GRV,  \
-    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC, KC_DEL,  \
-    KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  KC_ENT,  KC_PGUP, \
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_RSFT, KC_UP,   KC_PGDN, \
-    KC_LCTL, KC_LGUI, KC_LALT, KC_SPC,  KC_SPC,  KC_SPC,  KC_SPC,  KC_SPC,  KC_SPC,  KC_RALT, KC_RCTL, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT  \
+ [_BASE] = LAYOUT_ortho_5x15( \
+    KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,     KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_GRV,  \
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,     KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC, KC_DEL,  \
+    KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,     KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  KC_ENT,  KC_PGUP, \
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM,  KC_DOT,  KC_SLSH, KC_RSFT, KC_RSFT, KC_UP,   KC_PGDN, \
+    KC_LCTL, KC_LGUI, KC_LALT, KC_SPC,  KC_SPC,  KC_SPC,  KC_SPC,  KC_SPC,  KC_RALT,  KC_RGUI, KC_RCTL, MO(_FN), KC_LEFT, KC_DOWN, KC_RGHT  \
+  ),
+  [_FN] = LAYOUT_ortho_5x15( \
+    _______,  KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6,     KC_F7,     KC_F8,     KC_F9,      KC_F10,     KC_F11,     KC_F12,     _______,    _______,
+    _______,  _______,    _______,    _______,    _______,    _______,    _______,   _______,   _______,   _______,    _______,    _______,    _______,    _______,    _______,
+    _______,  _______,    _______,    _______,    _______,    _______,    _______,   _______,   _______,   _______,    _______,    _______,    _______,    KC_MPLY,    _______,
+    _______,  _______,    _______,    _______,    _______,    MD_BOOT,    _______,   _______,   _______,   _______,    _______,    _______,    _______,    KC_VOLU,    _______,
+    _______,  _______,    _______,    _______,    _______,    _______,    _______,   _______,   _______,   _______,    _______,    _______,    KC_MRWD,    KC_VOLD,    KC_MFFD
   ),
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  static uint32_t key_timer;
+
   switch (keycode) {
-    case QMKBEST:
+    case MD_BOOT:
       if (record->event.pressed) {
-        // when keycode QMKBEST is pressed
-        SEND_STRING("QMK is the best thing ever!");
+          key_timer = timer_read32();
       } else {
-        // when keycode QMKBEST is released
+          if (timer_elapsed32(key_timer) >= 500) {
+              reset_keyboard();
+          }
       }
-      break;
-    case QMKURL:
-      if (record->event.pressed) {
-        // when keycode QMKURL is pressed
-        SEND_STRING("https://qmk.fm/" SS_TAP(X_ENTER));
-      } else {
-        // when keycode QMKURL is released
-      }
-      break;
+      return false;
   }
   return true;
 }
--- keyboards/idobo/keymaps/matt/keymap.c
+++ keyboards/idobo/keymaps/matt/keymap.c
@@ -17,12 +17,6 @@
 
 #include "macros.h"
 
-// Defines the keycodes used by our macros in process_record_user
-enum custom_keycodes {
-  SHRUG = NEW_SAFE_RANGE,
-  ESHRUG
-};
-
 #define _QW 0
 #define _FN 1
 #define _MOVE 2
@@ -39,12 +33,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_FN] = LAYOUT_ortho_5x15( \
     _______,  KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6,     KC_F7,     KC_F8,     KC_F9,      KC_F10,     KC_F11,     KC_F12,     _______,    KC_DEL,
     _______,  _______,    _______,    _______,    _______,    _______,    _______,   _______,   _______,   _______,    _______,    _______,    _______,    _______,    _______,
-    _______,  _______,    _______,    _______,    _______,    _______,    RGB_MOD,   RGB_M_P,   _______,   _______,    _______,    _______,    _______,    _______,    _______,
-    _______,  _______,    _______,    _______,    _______,    MD_BOOT,    RGB_HUI,   _______,   _______,   _______,    _______,    _______,    _______,    _______,    _______,
-    _______,  _______,    _______,    _______,    _______,    _______,    RGB_HUD,   _______,   _______,   _______,    _______,    _______,    _______,    _______,    _______
+    _______,  _______,    _______,    _______,    _______,    _______,    _______,   _______,   _______,   _______,    _______,    _______,    _______,    _______,    _______,
+    _______,  _______,    _______,    _______,    _______,    _______,    _______,   _______,   _______,   _______,    _______,    _______,    _______,    _______,    _______,
+    _______,  _______,    _______,    _______,    _______,    _______,    _______,   _______,   _______,   _______,    _______,    _______,    _______,    _______,    _______
   ),
   [_MOVE] = LAYOUT_ortho_5x15( \
-    _______,  _______,    KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,     KC_F6,     KC_F7,     KC_F8,      KC_F9,      KC_F10,     KC_F11,     KC_F12,     KC_DEL,
+    _______,  KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,     KC_F6,     KC_F7,     KC_F8,      KC_F9,      KC_F10,     KC_F11,     KC_F12,     _______,    KC_DEL,
     _______,  _______,    _______,    _______,    _______,    _______,    _______,   _______,   _______,   _______,    _______,    _______,    _______,    _______,    _______,
     _______,  _______,    _______,    _______,    _______,    _______,    _______,   _______,   KC_LEFT,   KC_DOWN,    KC_UP,      KC_RGHT,    _______,    _______,    _______,
     _______,  _______,    _______,    _______,    _______,    _______,    KC_MRWD,   _______,   _______,   _______,    _______,    _______,    _______,    _______,    _______,
@@ -63,28 +57,15 @@ bool process_record_keymap(uint16_t keycode, keyrecord_t *record) {
   static uint32_t key_timer;
 
   switch (keycode) {
-    case SHRUG:
-      if (record->event.pressed) {
-        SEND_STRING("shrug");
-      } else {
-      }
-      break;
-    case ESHRUG:
-      if (record->event.pressed) {
-        SEND_STRING("eshrug");
-      } else {
-      }
-      break;
     case MD_BOOT:
       if (record->event.pressed) {
-          key_timer = timer_read32();
+        key_timer = timer_read32();
       } else {
-          if (timer_elapsed32(key_timer) >= 500) {
-              reset_keyboard();
-          }
+        if (timer_elapsed32(key_timer) >= 500) {
+          reset_keyboard();
+        }
       }
       return false;
-
   }
 
   return true;
